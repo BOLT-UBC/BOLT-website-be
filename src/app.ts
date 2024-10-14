f318d3534ca1a99dc4f825702b01182cc42dd29a
Kezia <roselinakeziarijadi@gmail.com>
2024-10-14 16:29:00 -0700

    setup auth0

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,27 @@
 import express from "express";
+import dotenv from "dotenv";
+import { auth, requiresAuth } from "express-openid-connect";
 import { createClient } from "@supabase/supabase-js";
+
 import welcomeRoutes from "./routes/welcome";
+import profileRouter from "./routes/profile";
+
+dotenv.config();
 
 const app = express();
+
+// Auth0
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.AUTH0_SECRET,
+  baseURL: process.env.AUTH0_BASE_URL,
+  clientID: process.env.AUTH0_CLIENT_ID,
+  issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
 
 // Initialize Supabase client
 const supabaseUrl = process.env.SUPABASE_URL
@@ -14,5 +33,6 @@ app.get('/', (req, res) => {
 });
 
 app.get('/welcome', welcomeRoutes);
+app.get('/profile', requiresAuth(), profileRouter);
 
 export default app;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import app from './app';
-import * as dotenv from 'dotenv';
-
-dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
