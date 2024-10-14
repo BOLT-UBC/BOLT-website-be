629810875a52c4d75a0e72cee3b5760913c71fca
Kezia <roselinakeziarijadi@gmail.com>
2024-10-14 15:46:39 -0700

    set up supabase

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
+import { createClient } from "@supabase/supabase-js";
 import welcomeRoutes from "./routes/welcome";
 
 const app = express();
 
+// Initialize Supabase client
+const supabaseUrl = process.env.SUPABASE_URL
+const supabaseKey = process.env.SUPABASE_ANON_KEY
+export const supabase = createClient(supabaseUrl as string, supabaseKey as string)
+
 app.get('/', (req, res) => {
-    res.status(200);
-    res.send("Welcome to server URL.");
-})
+  res.send("Hello, World!");
+});
 
 app.get('/welcome', welcomeRoutes);
 
